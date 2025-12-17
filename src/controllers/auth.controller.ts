bd30554b5c4653a6ce67cf5578f4f04b0aa0432d
Aril Harlih <aurielharlih9213@gmail.com>
2025-12-17 15:07:11 +0700

    update auth when user nonactive

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -8,8 +8,18 @@ export const login = async (req: Request, res: Response) => {
   if (!username || !password)
     return res.status(400).json({ message: "username & password required" });
 
-  const user = await prisma.user.findUnique({ where: { username } });
+  const user = await prisma.user.findUnique({
+    where: { username },
+    include: { pegawai: true },
+  });
+
   if (!user) return res.status(401).json({ message: "Invalid credentials" });
+
+  if (!user.isActive || (user.pegawai && user.pegawai?.status !== "active")) {
+    return res
+      .status(401)
+      .json({ message: "User atau data pegawai anda dinonaktifkan." });
+  }
 
   const ok = await comparePassword(password, user.password);
   if (!ok) return res.status(401).json({ message: "Invalid credentials" });
